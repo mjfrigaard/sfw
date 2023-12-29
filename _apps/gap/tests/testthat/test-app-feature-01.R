0114d3657b2ed000028070a42fcd31435bfe6a82
Martin Frigaard <mjfrigaard@pm.me>
2023-12-29 09:25:31 -0700

    updates to gap

--- _apps/gap/tests/testthat/test-app-feature-01.R
+++ _apps/gap/tests/testthat/test-app-feature-01.R
@@ -3,7 +3,7 @@ describe("Feature 1: Scatter plot data visualization dropdowns
            As a film data analyst
            I want to explore variables in the movie review data
            So that I can analyze relationships between movie reivew sources", {
-             
+
   describe("Scenario A: Change dropdown values for plotting
              Given the movie review application is loaded
              When I choose the variable [critics_score] for the x-axis
@@ -12,15 +12,15 @@ describe("Feature 1: Scatter plot data visualization dropdowns
         it("Then the scatter plot should show [critics_score] on the x-axis
              And the scatter plot should show [imdb_num_votes] on the y-axis
              And the points on the scatter plot should be colored by [genre]", {
-              app <- AppDriver$new(name = "feature-01-senario-a", 
+              app <- AppDriver$new(name = "feature-01-senario-a",
                                      height = 800, width = 1173)
                 app$set_inputs(`vars-y` = "imdb_num_votes")
                 app$set_inputs(`vars-x` = "critics_score")
                 app$set_inputs(`vars-z` = "genre")
                 app$expect_values()
        })
-     })
-             
+   })
+
   describe("Scenario B: Change dropdown values for plotting
               Given the movie review application is loaded
               When I choose the size of the points to be [3]
@@ -29,12 +29,12 @@ describe("Feature 1: Scatter plot data visualization dropdowns
          it("Then the size of the points on the scatter plot should be [3]
               And the opacity of the points on the scatter plot should be [0.7]
               And the title of the plot should be '[New plot title]'", {
-              app <- AppDriver$new(name = "feature-01-senario-b", 
+              app <- AppDriver$new(name = "feature-01-senario-b",
                                      height = 800, width = 1173)
                 app$set_inputs(`vars-alpha` = 0.7)
                 app$set_inputs(`vars-size` = 3)
                 app$set_inputs(`vars-plot_title` = "New plot title")
                 app$expect_values()
         })
-      })
+    })
 })
